e26eadcdf3d145447b086baef9f30ee49b818f52
RankoHata <41975337+RankoHata@users.noreply.github.com>
2018-09-02 09:50:58 +0800

    Deployment

--- app.py
+++ app.py
@@ -110,6 +110,7 @@ def create_app():
 
 
 if __name__ == '__main__':
-    create_app().run(host='0.0.0.0', port=12999)
+    application = create_app()
+    application.run(host='0.0.0.0', port=12999)
     # create_app().run(port=12999)
 
